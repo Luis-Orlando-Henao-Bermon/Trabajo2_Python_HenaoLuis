a3a7f77e22eb5c41bb7098354154291fcc721e58
Luis <roronoazoro3704@gmail.com>
2024-08-08 22:32:28 -0500

    :memo: finalizacion de readme y documentacion del codigo

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # Trabajo2_Python_HenaoLuis
+
+Un sistema integral de gestión que permite manejar todas las operaciones relacionadas con la administración de medicamentos, proveedores, empleados, pacientes y permite un registro de ventas y compras dentro del sistema.
+
+## Estructura 
+
+![image](https://github.com/user-attachments/assets/bc7d97c0-2801-4f44-b4f8-1a5bb27f69df)
+
+## Tecnologias usadas
+
+![Python](https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue)
+![json](https://img.shields.io/badge/json-5E5C5C?style=for-the-badge&logo=json&logoColor=white)
+
+## Archivos y funciones
+
+|Archivo|Funcion|
+|--|--|
+|**Compras.json**|Se registra el historial de todas las compras a proveedores realizadas|
+|**Empleados.json**|Estan registrados todos los empleados de la farmacia|
+|**Main.py**|Archivo principal en el con el cual se ejecuta todo el programa|
+|**Medicamentos.json**|Se lleva el registro de todos los medicamentos que hay junto con la cantida que hay en stock|
+|**Pacientes.json**|Estan registrados todos los pacientes de la farmacia|
+|**Proveedores.json**|Estan regitrados los proveedores de la farmacia|
+|**Ventas.json**|Se registra el historial de todas las ventas realizadas |
+
+## Instalación y Uso
+**Debes tener instalado visual studio code, python y git en su ultima version**
+- Para ello debes clonar el directorio del siguiente link de git hub https://github.com/Luis-Orlando-Henao-Bermon/Trabajo2_Python_HenaoLuis
+
+- Abre la terminal de git bash y allí escribe el siguiente comando:     
+
+``` bash 
+git clone https://github.com/Luis-Orlando-Henao-Bermon/Trabajo2_Python_HenaoLuis
+```
+- Abre visual studio code e instalas la extencion de python
+
+
+![image](https://github.com/user-attachments/assets/371fc5fb-ec7a-4c0e-a1ae-af9c21f1b78c)
+
+
+
+- Abres una nueva terminal de git dando siguiendo las siguientes instrucciones:
+![image](https://github.com/user-attachments/assets/65474d7f-4984-4b1c-81c4-3d18639ac3cc)
+
+
+![image](https://github.com/user-attachments/assets/e016b121-6b6d-41cf-884d-7b1d81dd2cb7)
+
+
+![image](https://github.com/user-attachments/assets/9647ae18-b5c8-40c9-a7d4-a7340392dd35)
+
+
+![image](https://github.com/user-attachments/assets/7ad541fc-88fd-4443-ad99-7c67777fbd1c)
+
+- Y finalmente ejecutas el siguiente comando en git bash
+```bash
+python main.py
+```
+
+## Créditos
+
+Desarrollado por: *Luis Orlando Henao Bermon*
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-from os import system
-from datetime import datetime
+import json #como se trabaja con archivos json hay que imortar la libreria  de json
+from os import system #se importa sytem para poder limpiar pantalla en git bash
+from datetime import datetime #se importa datetime para poder saber la hora en que se hacen los registros 
 
+
+#seccion para importar los archivos de json y poder trabajar con ellos 
 with open ("./Medicamentos.json", encoding="UTF-8") as file:
     medicamentos=json.load(file)
 with open ("./Compras.json", encoding="UTF-8") as file:
@@ -18,77 +20,85 @@ with open ("./Ventas.json", encoding="UTF-8") as file:
 
 bol=True
 system("clear")
-while bol==True:
-    system("clear")
+while bol==True: #se usa un bucle while para que se repita el menu hasta que sellecione la opcion 3 que termina el bucle
+    system("clear")# con esto se limpia consola en git bash
     opcion=int(input("==========Menu==========\n1. Registrar ventas\n2. Registrar compra\n3. Salir\nEscribe tu opcion\n"))
     system("clear")
     if opcion==1:
-        fechaVenta=str(datetime.now())
+        fechaVenta=str(datetime.now()) #se usa un str para poder leer el datetime.now() como entero y no tenga problemas al momento de exportarlo en el json
         print("----------Pacientes----------")
         
-        for i in pacientes:
+        for i in pacientes: #se usa un bucle for para que mire todos los pacientes y muestre los nombres con sus id
             print("ID:",i["id"],"------ Nombre:",i["nombre"])
             print("------------------------------------------")
         
         idPacienteV=int(input("Ingresa el ID del paciente al que se le realizara la venta\n"))
         system("clear")
-        for i in pacientes:
-            if i["id"]==idPacienteV:
+        
+        for i in pacientes:#despues de saber el id del paciente al que se le quiere hacer la venta se usa un bucle if para comprararlo con los id que hay en el json pacientes 
+            if i["id"]==idPacienteV: #si un paciente tiene el id ingresado por el usuario a la variable pacienteV se le agrega en un diccionario el nombre y direccion
                 pacienteV={"nombre":i["nombre"],"direccion":i["direccion"]}
         
         print("----------Empleados----------")
-        for i in empleados:
+        for i in empleados:# se usa un bucle for para que mire todos los empleados y muestre sus nombres y sus id
             print("ID:",i["id"],"------ Nombre:",i["nombre"])
             print("------------------------------------------")
         
         idEmpleadoV=int(input("Ingresa el ID del empleado que realizara la venta\n"))
-        for i in empleados:
-            if i["id"]==idEmpleadoV:
+        
+        for i in empleados: # despues de saber el id del empleado se usa un bucle for para mirar en el json de empleados y ver si un empleado tiene ese id 
+            if i["id"]==idEmpleadoV:#si un empleado tiene ese id a la variable empleadoV se le agrega en un diccionario el nombre y el cargo de ese empleado
                 empleadoV={"nombre":i["nombre"],"cargo":i["cargo"]}
         
         system("clear")
         print("----------Medicamentos----------")
-        for i in medicamentos:
+        
+        for i in medicamentos: # se usa un bucle for para mirar en el json de medicamentos y motrar los nombres, precios y cantidad en stock 
             print("ID:",i["id"],"------ Nombre:",i["nombre"],"------ Precio:",i["precio"],"------ Cantidad en stock:",i["stock"])
             print("------------------------------------------")
         
         idMedicamentoV=int(input("Ingresa el ID del medicamento que se va a vender\n"))
         cantidadMedicamentoV=int(input("Ingresa la cantidad de de medicamento que se va a vender\n"))
-        for i in medicamentos:
-            if i["id"]==idMedicamentoV:
+        
+        for i in medicamentos:# se usa un bucle for para mirar si algun medicamento tiene el id ingresado por el usuario
+            if i["id"]==idMedicamentoV:# si n medicamento tiene el mismo id ingresado por el usuario a la variable medicamentoV se le agrega en un diccionario el nombre de ese medicamento, la cantidad vendida y el precio de ese medicamento
                 medicamentoV={"nombreMedicamento":i["nombre"],"cantidadVendida":cantidadMedicamentoV,"precio":i["precio"]}
-                i["stock"]=i["stock"]-cantidadMedicamentoV
-
+                i["stock"]=i["stock"]-cantidadMedicamentoV # al json de medicamentos se le resta la cantidad de medicamento vendida
+        
+        #despues de saber los datos de la fecha en que se realiza la venta, paciente, empleado y medicamento se le agrega el json de ventas usando un .append a la variable que contiene el json de ventas
         ventas.append({"fechaVenta":fechaVenta,"paciente":pacienteV,"empleado":empleadoV,"medicamentosVendidos":medicamentoV})
         system("clear")
         input("Venta realizada con exito 😀\nPreciona Enter para continuar")
 
     elif opcion==2:
-        fechaCompra=str(datetime.now())
+
+        fechaCompra=str(datetime.now())#se usa un str para poder leer el datetime.now() como entero y no tenga problemas al momento de exportarlo en el json
+
         print("----------Proveedores----------")
-        for i in proveedores:
+        for i in proveedores:# se usa un bucle for para mirar en el json de proveedores y mostrar el id y nombre 
             print("ID:",i["id"],"-------- Nombre:",i["nombre"])
 
         idProveedor=int(input("Ingresa el id del proveedor al que se le realizara la compra\n"))
 
-        for i in proveedores:
-            if i["id"]==idProveedor:
+        for i in proveedores:#se usa un bucle for para mirar si algun proveedor tiene ese id 
+            if i["id"]==idProveedor: #si algun proveedor tiene ese id a la variable proveedor se le agrega en un diccionario el nombre y contacto de ese proveedor
                 proveedor={"nombre":i["nombre"],"contacto":i["contacto"]}
 
         system("clear")
         print("----------Medicamentos----------")
-        for i in medicamentos:
+        for i in medicamentos:# se usa un bucle for para mirar en el json de medicamentos y motrar los nombres, precios y cantidad en stock 
             print("ID:",i["id"],"------ Nombre:",i["nombre"],"------ Precio:",i["precio"],"------ Cantidad en stock:",i["stock"])
             print("------------------------------------------")
         
         idMedicamentoC=int(input("Ingresa el ID del medicamento que se va a comprar\n"))
         cantidadMedicamentoC=int(input("Ingresa la cantidad de de medicamento que se va a comprar\n"))
         precioCompra=int(input("Ingrese el precio de compra del medicamento\n"))
-        for i in medicamentos:
-            if i["id"]==idMedicamentoC:
+        for i in medicamentos:# se usa un bucle for para mirar si algun medicamento tiene el id ingresado por el usuario
+            if i["id"]==idMedicamentoC:# si n medicamento tiene el mismo id ingresado por el usuario a la variable medicamentoV se le agrega en un diccionario el nombre de ese medicamento, la cantidad comprada y el precio de compra de ese medicamento
                 medicamentoC={"nombreMedicamento":i["nombre"],"cantidadComprada":cantidadMedicamentoC,"precioCompra":precioCompra}
-                i["stock"]=i["stock"]+cantidadMedicamentoC
+                i["stock"]=i["stock"]+cantidadMedicamentoC #al json de medicamentos se le suma la cantidad de medicamento comprada 
         
+        # al json de compras se le agregas los datos obtenidos anteriormente de la fecha de compra, proveedor y medicamnto comprado
         compras.append({"fechaCompra":fechaCompra,"proveedor":proveedor,"medicamentosComprados":medicamentoC})
 
         system("clear")
@@ -97,11 +107,8 @@ while bol==True:
     elif opcion==3:
         print("Gracias por usar el programa ♥")
         bol=False
-# hola=str(datetime.now())
-# print(hola)
 
-
-
+# seccion de exportacion de archivos json con esto se guardan todos los cambios en los archivos json
 jsonMedicamentos=json.dumps(medicamentos)
 with open("./Medicamentos.json","w") as file:
     file.write(jsonMedicamentos)
@@ -120,3 +127,5 @@ with open("./Proveedores.json","w") as file:
 jsonVentas=json.dumps(ventas)
 with open("./Ventas.json","w") as file:
     file.write(jsonVentas)
+
+# Desarrollado por Luis Henao
